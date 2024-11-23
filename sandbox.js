27ec67a20f8bc368a886ea592945f9f74d19bdae
Sasan <sasan.nikjoo.83@gmail.com>
2024-11-23 06:42:44 +0330

    new learning session

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,42 @@
+//8
+// let greet: Function;
+// greet = () =>{
+//     console.log("hello peter");
+// }
+// greet();
+// //c is optional and has a defualt value
+// const addfunc = (a: number, b: number, c: number| string = 10) =>{
+//     console.log(a + b);
+//     console.log(c);
+// }
+// addfunc(2, 3, 13)
+//---------------------------------------------------
+//9
+//use the types beside writing the selected types all the time
+// type StringOrNum = string | number;
+// type ObjWithName = {name: string, uid: StringOrNum};
+// const logDetails = (uid: StringOrNum, item: string)=>{
+//     console.log('${item} has a uid of ${uid}');
+// }
+// const greet = (user: ObjWithName) =>{
+//     console.log('${user.name} says hello')
+// }
+// logDetails(4012262061, "book");
+// greet({name: "sasan", uid:4012262061});
+//---------------------------------------------------
+//10
+// Function Signatures
+//ex1
 var greet;
-greet = function () {
-    console.log("hello peter");
+greet = function (name, greeting) {
+    console.log('${name} says ${greeting}');
 };
-greet();
-//c is optional and has a defualt value
-var addfunc = function (a, b, c) {
-    if (c === void 0) { c = 10; }
-    console.log(a + b);
-    console.log(c);
+//ex2
+var sumie;
+sumie = function (num1, num2, num3) {
+    return num1 + num2 + num3;
 };
-addfunc(2, 3, 13);
+var logDetails;
+logDetails = function (person) {
+    console.log("${person.name} is ${person.age} years old");
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,16 +1,61 @@
-let greet: Function;
+//8
+// let greet: Function;
 
-greet = () =>{
-    console.log("hello peter");
+// greet = () =>{
+//     console.log("hello peter");
+// }
+
+// greet();
+
+
+// //c is optional and has a defualt value
+// const addfunc = (a: number, b: number, c: number| string = 10) =>{
+//     console.log(a + b);
+//     console.log(c);
+// }
+
+// addfunc(2, 3, 13)
+
+//---------------------------------------------------
+//9
+
+//use the types beside writing the selected types all the time
+// type StringOrNum = string | number;
+// type ObjWithName = {name: string, uid: StringOrNum};
+
+// const logDetails = (uid: StringOrNum, item: string)=>{
+//     console.log('${item} has a uid of ${uid}');
+// }
+
+// const greet = (user: ObjWithName) =>{
+//     console.log('${user.name} says hello')
+// }
+
+// logDetails(4012262061, "book");
+// greet({name: "sasan", uid:4012262061});
+
+//---------------------------------------------------
+//10
+// Function Signatures
+
+//ex1
+let greet: (a: string, b:string)=> void;
+greet = (name: string, greeting: string)=>{
+    console.log('${name} says ${greeting}')
 }
 
-greet();
+//ex2
+let sumie: (a: number, b: number, c: number) => number;
 
-
-//c is optional and has a defualt value
-const addfunc = (a: number, b: number, c: number| string = 10) =>{
-    console.log(a + b);
-    console.log(c);
+sumie = (num1: number, num2: number, num3: number) =>{
+    return num1 + num2 + num3;
 }
 
-addfunc(2, 3, 13)
+//ex3
+type Person = {name: string, age: number};
+
+let logDetails: (obj: Person) => void;
+
+logDetails = (person: Person) =>{
+    console.log("${person.name} is ${person.age} years old");
+}
