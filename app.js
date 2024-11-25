c287a60c7c7e953e26981638b9a3879f9e730e4c
Sasan <sasan.nikjoo.83@gmail.com>
2024-11-25 13:53:01 +0330

    new lesson

---
+++ app.js
@@ -0,0 +1,10 @@
+// const anchor = document.querySelector('a');
+// console.log(anchor?.href);
+// const form = document.querySelector('form')!;
+var form = document.querySelector('.new-item-form');
+console.log(form.children);
+//inputs
+var type = document.querySelector('#type');
+var toform = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
